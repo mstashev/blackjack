0fafb24b3eda8f2b28241597ac777de52f087182
Michael Stashevsky <mstashev@gmail.com>
2017-02-09 16:53:05 -0500

    Starting Adventure

---
+++ adventure/blackjack.rb
@@ -0,0 +1,165 @@
+require 'tty'
+require_relative 'card'
+require_relative 'deck'
+
+class Blackjack
+
+  attr_accessor :p1hand,
+                :househand,
+                :prompt,
+                :deck,
+                :players,
+                :player_input,
+                :house_total,
+                :player_total
+
+  def initialize
+    @prompt = TTY::Prompt.new
+    @deck = Deck.new
+    @p1hand = []
+    @househand = []
+    @players = [@p1hand, @househand]
+    @player_input = nil
+    @house_total = 0
+    @player_total = 0
+  end
+
+  def deal(players)
+    2.times do
+      players.each do |player|
+        player << deck.draw
+      end
+    end
+  end
+
+  def play
+    deal(players)
+    # puts "Player hand: #{p1hand.inspect}"
+    # puts "House hand: #{househand.inspect}"
+
+    until player_input == "Stay" || is_over_21?(p1hand)
+      playerturn(p1hand)
+    end
+
+    if house_total < player_total
+      if player_total > 21
+        puts "House wins!"
+      else
+        puts "You win!"
+      end
+    elsif player_total < house_total
+      if  house_total > 21
+        puts "Player wins!"
+      else
+        puts "House wins!"
+      end
+    else
+      puts "You tied. House rules say, You win!"
+    end
+  end
+
+  def houseturn(hand)
+    house_total = 0
+    hand.each do |card|
+      house_total += card.value
+    end
+    if is_over_21?(househand) == true
+      puts "Bust, Player wins"
+      exit
+    elsif is_21?(househand) == true
+      puts "Blackjack"
+      exit
+    end
+    if house_total < 16
+      hit(househand)
+      # puts househand.inspect
+      houseturn(househand)
+    else
+      puts "House has: "
+      househand.each do |card|
+        puts "#{card.face} of #{card.suit}"
+      end
+      puts "House hand total is #{house_total}."
+      stay(house_total, "House")
+    end
+  end
+
+  def playerturn(hand)
+    player_total = 0
+    p1hand.each do |card|
+      player_total += card.value
+    end
+    puts "You have: "
+    p1hand.each do |card|
+      puts "#{card.face} of #{card.suit}"
+    end
+    puts "Your hand total is #{player_total}."
+    if is_over_21?(p1hand) == true
+      puts "Bust, dealer wins"
+      exit
+    elsif is_21?(p1hand) == true
+      puts "Blackjack"
+      exit
+    else
+      pick_an_option(player_total)
+    end
+  end
+
+
+  def pick_an_option(response = nil, player_total)
+    response = prompt.select("Would you like.", %w(Hit Stay)).downcase
+
+    case response
+    when "hit"
+      hit(p1hand)
+      playerturn(p1hand)
+    when "stay"
+      stay(player_total, "Player")
+      houseturn(househand)
+    end
+  end
+
+  def is_over_21?(hand)
+    hand_total = 0
+    hand.each do |card|
+      hand_total += card.value
+    end
+    if hand_total <= 21
+      false
+    else
+      true
+    end
+  end
+
+  def is_21?(hand)
+    hand_total = 0
+    hand.each do |card|
+      hand_total += card.value
+    end
+    if hand_total == 21
+      true
+    else
+      false
+    end
+  end
+
+  def hit(hand)
+    hand << deck.draw
+  end
+
+  def stay(total, who)
+    @player_input = "Stay"
+    if who == "Player"
+      @player_total  = total
+    elsif who == "House"
+      @house_total = total
+    else
+      puts "Error"
+      exit
+    end
+  end
+
+
+
+end
+Blackjack.new.play
---
+++ adventure/deck_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+
+require_relative 'deck'
+
+class DeckTest < MiniTest::Test
+
+  def test_num_cards_in_deck
+    deck = Deck.new
+    assert_equal 52, deck.cards.length
+    assert deck.cards.all?{|c| c.is_a? Card}
+  end
+
+  def test_gets_shuffled_when_created
+    deck1 = Deck.new
+    deck2 = Deck.new
+    refute_equal deck1.cards, deck2.cards
+  end
+
+  def test_four_each_face_card
+    deck = Deck.new
+    faces = (2..10).to_a + %w(J Q K A)
+    faces.each do |face|
+      assert deck.cards.count{|card| card.face == face} == 4
+    end
+  end
+
+  def test_thirteen_of_each_suit
+    deck = Deck.new
+    suits = %w(Clubs Spades Diamonds Hearts)
+    suits.each do |suit|
+      assert deck.cards.count { |card| card.suit.include?(suit)} == 13
+    end
+  end
+end
